542b6606bd944decbf7452368c937df7f77f9516
kabiskac <stubiflex.junior@gmail.com>
2021-06-20 01:20:29 +0200

    Fixed characters.

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,7 @@
 import anime from './functions/anime';
 import manga from './functions/manga';
 import genre from './functions/genre';
+import character from './functions/character'
 import itemById from './functions/itemById';
 
-export default { anime, manga, genre, itemById };
+export default { anime, manga, genre, character, itemById };
